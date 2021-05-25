7e5680ebb96e39ebf733b5e4fc0fa95e14488cbb
clint <ucmodulus@gmail.com>
2021-05-25 11:50:07 +0100

    Removed Backend

--- views/home.php
+++ views/home.php
@@ -5,63 +5,12 @@
   <?php require_once 'includes/links.php'; ?>
 </head>
 <body class="bg-triangles">
-  <!-- Preloader -->
-  <div class="preloader">
-    <div class="preloader__wrap">
-      <div class="circle-pulse">
-        <div class="circle-pulse__1"></div>
-        <div class="circle-pulse__2"></div>
-      </div>
-      <div class="preloader__progress"><span></span></div>
-    </div>
-  </div>
 
   <main class="main">
     <div class="container gutter-top">
       <div class="row sticky-parent">
-        <!-- Sidebar -->
-        <aside class="col-12 col-md-12 col-xl-3">
-          <div class="sidebar box shadow pb-0 sticky-column">
-            <svg class="avatar avatar--180" viewBox="0 0 188 188">
-              <g class="avatar__box">
-                <image xlink:href="assets/img/avatar-1.jpg" height="100%" width="100%" />
-              </g>
-            </svg>
-            <div class="text-center">
-              <h3 class="title title--h3 sidebar__user-name"><span class="weight--500">Felecia</span> Brown</h3>
-              <div class="badge badge--dark">Creative Director</div>
 
-              <!-- Social -->
-              <div class="social">
-                <a class="social__link" href="https://www.facebook.com/"><i class="font-icon icon-facebook"></i></a>
-                <a class="social__link" href="https://www.behance.com/"><i class="font-icon icon-twitter"></i></a>
-                <a class="social__link" href="https://www.linkedin.com/"><i class="font-icon icon-linkedin2"></i></a>
-              </div>
-            </div>
-
-            <div class="sidebar__info box-inner">
-              <ul class="contacts-block">
-                <li class="contacts-block__item" data-toggle="tooltip" data-placement="top" title="Birthday">
-                  <i class="font-icon icon-calendar"></i>March 12, 1995
-                </li>
-                <li class="contacts-block__item" data-toggle="tooltip" data-placement="top" title="Address">
-                  <i class="font-icon icon-location"></i>Hong Kong, China
-                </li>
-                <li class="contacts-block__item" data-toggle="tooltip" data-placement="top" title="E-mail">
-                  <a href="mailto:example@mail.com"><i class="font-icon icon-envelope"></i>example@mail.com</a>
-                </li>
-                <li class="contacts-block__item" data-toggle="tooltip" data-placement="top" title="Phone">
-                  <i class="font-icon icon-phone"></i>+1 (070) 123-4567
-                </li>
-                <li class="contacts-block__item" data-toggle="tooltip" data-placement="top" title="Skype">
-                  <a href="skype:skype-example"><i class="font-icon icon-skype"></i>Felecia_Brown</a>
-                </li>
-              </ul>
-
-              <a class="btn" href="#"><i class="font-icon icon-download"></i> Download CV</a>
-            </div>
-          </div>
-        </aside>
+        <?php require_once 'includes/sidebar.php'; ?>
 
         <!-- Content -->
         <div class="col-12 col-md-12 col-xl-9">
@@ -74,15 +23,8 @@
                 <div class="line"></div>
               </div>
             </div>
-            <div class="inner-menu js-menu">
-              <ul class="nav">
-                <li class="nav__item"><a class="active" href="about.html">About</a></li>
-                <li class="nav__item"><a href="resume.html">Resume</a></li>
-                <li class="nav__item"><a href="portfolio.html">Portfolio</a></li>
-                <li class="nav__item"><a href="blog.html">Blog</a></li>
-                <li class="nav__item"><a href="contact.html">Contact</a></li>
-              </ul>
-            </div>
+            <?php require_once 'includes/header.php'; ?>
+
 
             <!-- About -->
             <div class="pb-0 pb-sm-2">
@@ -245,17 +187,7 @@
     </div>
   </main>
 
-  <div class="back-to-top"></div>
-
-  <!-- SVG masks -->
-  <svg class="svg-defs">
-    <clipPath id="avatar-box">
-      <path d="M1.85379 38.4859C2.9221 18.6653 18.6653 2.92275 38.4858 1.85453 56.0986.905299 77.2792 0 94 0c16.721 0 37.901.905299 55.514 1.85453 19.821 1.06822 35.564 16.81077 36.632 36.63137C187.095 56.0922 188 77.267 188 94c0 16.733-.905 37.908-1.854 55.514-1.068 19.821-16.811 35.563-36.632 36.631C131.901 187.095 110.721 188 94 188c-16.7208 0-37.9014-.905-55.5142-1.855-19.8205-1.068-35.5637-16.81-36.63201-36.631C.904831 131.908 0 110.733 0 94c0-16.733.904831-37.9078 1.85379-55.5141z"/>
-    </clipPath>
-    <clipPath id="avatar-hexagon">
-      <path d="M0 27.2891c0-4.6662 2.4889-8.976 6.52491-11.2986L31.308 1.72845c3.98-2.290382 8.8697-2.305446 12.8637-.03963l25.234 14.31558C73.4807 18.3162 76 22.6478 76 27.3426V56.684c0 4.6805-2.5041 9.0013-6.5597 11.3186L44.4317 82.2915c-3.9869 2.278-8.8765 2.278-12.8634 0L6.55974 68.0026C2.50414 65.6853 0 61.3645 0 56.684V27.2891z"/>
-    </clipPath>
-  </svg>
+  <?php require_once 'includes/footer.php'; ?>
 
 </body>
 </html>
