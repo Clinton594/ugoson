cea055ffc3ccc4387df1cd6c8d78bc70ea28fca2
Onuigbo Clinton <32768102+Clinton594@users.noreply.github.com>
2022-03-16 22:21:50 +0100

    Removed php

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<div class="back-to-top"></div>
-
-
-<!-- JavaScripts -->
-<script src="<?=$uri->site?>assets/js/jquery-3.4.1.min.js"></script>
-<script src="<?=$uri->site?>assets/js/plugins.js"></script>
-<script src="<?=$uri->site?>assets/js/common.js<?=$cache_control?>"></script>
-
-<script src="<?=$uri->site?>assets/demo/plugins-demo.js<?=$cache_control?>"></script>
