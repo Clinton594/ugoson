e1e94ccd4e6932b3ff6ddda22835975860347e07
Onuigbo Clinton <32768102+Clinton594@users.noreply.github.com>
2022-04-18 21:43:24 +0100

    New updates

---
+++ src/statics/profile.js
@@ -0,0 +1,145 @@
+const profile = {
+  name: "Clinton Ugochukwu Onuigbo",
+  role: "Fullstack Web Developer",
+  email: "clinton.onuigbo8310@gmail.com",
+  picture: "assets/images/IMG_-ywdefa.jpg",
+  website: "",
+  phones: {
+    airtel: "+2348075941561",
+    mtn: "+2347034458310",
+  },
+  bio: "I am a Full-Stack web developer. I have been in active development for seven (7) years working with PHP, JavaScripts, Node Js and I have built applications ranging from School management, Hotel management, Enterprise web apps, APIs development, Database development and management. Currently I am working on BlockChain Decentralized Applications. I seek to use my key strengths to work for a “growth-conscious” firm while developing on my areas of low competencies",
+  features: [
+    {
+      image: "assets/icons/browser-web-development-svgrepo-com.svg",
+      title: "BackEnd Web Development",
+      description:
+        "High-quality and professional development of websites at the professional level. I currently work with Hackpiy as the CTO.",
+    },
+    {
+      image: "assets/icons/web-design-svgrepo-com.svg",
+      title: "Frontend Web Development",
+      description: "I am not a full-time UI/UX designer but i deploy very good user friendly interfaces.",
+    },
+    {
+      image: "assets/icons/link-chain-svgrepo-com.svg",
+      title: "BlockChain Development",
+      description:
+        "I am a mid-level BlockChain Developer (Solidity), i'm currently expanding into other blockchain technologies",
+    },
+  ],
+  keyCompetence: [
+    {
+      title: "Backend",
+      list: [
+        {
+          language: "Advanced PHP",
+          level: 95,
+        },
+        {
+          language: "Node JS",
+          level: 89,
+        },
+        {
+          language: "Solidity",
+          level: 50,
+        },
+        {
+          language: "Rest API",
+          level: 90,
+        },
+      ],
+    },
+    {
+      title: "Frontend",
+      list: [
+        {
+          language: "React JS",
+          level: 96,
+        },
+        {
+          language: "Pure JQuery JS",
+          level: 98,
+        },
+        {
+          language: "Pure Vanilla JS (ES6)",
+          level: 95,
+        },
+        {
+          language: "HTML, XML, SVG & CSS",
+          level: 98,
+        },
+      ],
+    },
+    {
+      title: "DataBases",
+      list: [
+        {
+          language: "MySQL Database",
+          level: 89,
+        },
+        {
+          language: "FireBase",
+          level: 45,
+        },
+        {
+          language: "MongoDB",
+          level: 66,
+        },
+      ],
+    },
+    {
+      title: "Operations",
+      list: [
+        {
+          language: "Git & GitHub",
+          level: 97,
+        },
+        {
+          language: "Hostinger CI/CD",
+          level: 100,
+        },
+        {
+          language: "AWS",
+          level: 65,
+        },
+      ],
+    },
+  ],
+  portfolio: [
+    {
+      image: "assets/images/ahiamoni.png",
+      title: "Ahiamoni",
+      category: "E-Commerce",
+      description: "Online Stores, Product Listing and Adverts",
+      link: "https://ahiamoni.com",
+      disabled: true,
+    },
+    {
+      image: "assets/images/geohomes.png",
+      title: "GeoHomes Group",
+      category: "Real Estates",
+      description: "Residential Developments, Commercial Developments and Estate Planing",
+      link: "https://geohomesgroup.com",
+      disabled: false,
+    },
+    {
+      image: "assets/images/lendtrx-wallet.png",
+      title: "LendTRX Wallet",
+      category: "Blockchain",
+      description: "OffChain Wallet, Send, Recieve and Swap Crypto.",
+      link: "https://lendtrx.com",
+      disabled: false,
+    },
+    {
+      image: "assets/images/avaxdot.png",
+      title: "Avaxdot",
+      category: "Finance",
+      description: "Crypto Investments, Forex Investments",
+      link: "https://avaxdot.com",
+      disabled: false,
+    },
+  ],
+};
+
+export default profile;
